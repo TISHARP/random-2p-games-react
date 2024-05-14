517d5c34cdc0b4d554b39d05cc95f9e473d3c6e2
TISHARP <sharpinctechandprogramming@gmail.com>
2024-05-13 20:32:19 -0600

    Updated Tic-Tac-Toe to have a tic-tac-toe icon

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,11 +1,12 @@
 import { gameInfo } from "../../types/games";
 import { useNavigate } from "react-router-dom";
+import TicTacToeIcon from '../../assets/tictactoe.webp';
 import "./HomePage.css";
 
 const HomePage = () => {
     const navigate = useNavigate();
     const games = [
-        {name: "Tic Tac Toe", to: '/games/tic-tac-toe', gicon: null},
+        {name: "Tic Tac Toe", to: '/games/tic-tac-toe', gicon: TicTacToeIcon}
     ]
     return (
         <div className="game-section-page">
@@ -13,7 +14,7 @@ const HomePage = () => {
                 {games.map((gameEntry:gameInfo)=>(
                     <div key={gameEntry.to} onClick={()=>navigate(gameEntry.to)} className={"game-card "+(gameEntry.className?gameEntry.className:'')}>
                         <h2>{gameEntry.name}</h2>
-                        {gameEntry.gicon ? <img src={gameEntry.gicon} alt={"Image depicting "+gameEntry.name} /> : ''}
+                        {gameEntry.gicon ? <img src={gameEntry.gicon} alt={"Image depicting "+gameEntry.name} style={{maxWidth: '200px'}}/> : ''}
                     </div>
                 ))}
             </div>
